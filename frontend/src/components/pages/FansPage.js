23cf533973f614db00a0af7845247966f2875014
Laura Vera <lauv2210@gmail.com>
2024-02-29 10:54:09 -0300

    Agrego componentes InspiracionPage, FansPage y agrego sus estilos CSS

---
+++ frontend/src/components/pages/FansPage.js
@@ -0,0 +1,164 @@
+import "../../styles/components/pages/FansPage.css";
+
+const FansPage = (props) => {
+  return (
+    <div className="centrar1">
+      <main className="activo">
+        <h2 className="centrar2">Tus ideas</h2>
+        <p className="centrar2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex quidem
+          repellendus voluptatibus dicta deleniti, sit, voluptates placeat
+          molestias vel quibusdam corrupti perferendis tempora cupiditate
+          dolores quas qui aliquid delectus similique nostrum laboriosam dolor
+          deserunt quam?
+        </p>
+        <div id="contenedor-ideas">
+          <div className="idea">
+            <div className="imgs-ideas">
+              <img src="/images/ideas/cat.jpg" alt="Gato" />
+            </div>
+            <i
+              className="fa-solid fa-face-smile fa-2xl"
+              style={{ color: "#ffd43b" }}
+            ></i>
+            <h3>Nombre Fan 1</h3>
+          </div>
+          <div className="idea">
+            <div className="imgs-ideas">
+              <img src="/images/ideas/cherry-socks.jpg" alt="Medias Cerezas" />
+              <img src="images/ideas/heart-socks.jpg" alt="Medias Corazones" />
+            </div>
+            <i
+              className="fa-solid fa-face-smile fa-2xl"
+              style={{ color: "#ffd43b" }}
+            ></i>
+            <h3>Nombre Fan 2</h3>
+          </div>
+          <div className="idea">
+            <div className="imgs-ideas">
+              <img src="/images/ideas/coffee-mugs.jpg" alt="Tazas de Café" />
+              <img src="/images/ideas/hello-coffee.jpg" alt="Hello Coffee" />
+            </div>
+            <i
+              className="fa-solid fa-face-smile fa-2xl"
+              style={{ color: "#ffd43b" }}
+            ></i>
+            <h3>Nombre Fan 3</h3>
+          </div>
+          <div className="idea">
+            <div className="imgs-ideas">
+              <img src="/images/ideas/dog.jpg" alt="Perro" />
+              <img
+                src="/images/ideas/dog-starbucks.jpg"
+                alt="Perro Starbucks"
+              />
+            </div>
+            <i
+              className="fa-solid fa-face-smile fa-2xl"
+              style={{ color: "#ffd43b" }}
+            ></i>
+            <h3>Nombre Fan 4</h3>
+          </div>
+          <div className="idea">
+            <div className="imgs-ideas">
+              <img
+                src="/images/ideas/flowers-jacket.jpg"
+                alt="Campera con Flores"
+              />
+              <img
+                src="/images/ideas/flowers-jacket2.jpg"
+                alt="Campera Rosa con Flores"
+              />
+            </div>
+            <i
+              className="fa-solid fa-face-smile fa-2xl"
+              style={{ color: "#ffd43b" }}
+            ></i>
+            <h3>Nombre Fan 5</h3>
+          </div>
+          <div className="idea">
+            <div className="imgs-ideas">
+              <img src="/images/ideas/kawaii.jpg" alt="Kawaii Beanie" />
+            </div>
+            <i
+              className="fa-solid fa-face-smile fa-2xl"
+              style={{ color: "#ffd43b" }}
+            ></i>
+            <h3>Nombre Fan 6</h3>
+          </div>
+          <div className="idea">
+            <div className="imgs-ideas">
+              <img src="/images/ideas/lemon.jpg" alt="Limón" />
+              <img src="/images/ideas/strawberry.jpg" alt="Frutilla" />
+            </div>
+            <i
+              className="fa-solid fa-face-smile fa-2xl"
+              style={{ color: "#ffd43b" }}
+            ></i>
+            <h3>Nombre Fan 7</h3>
+          </div>
+          <div className="idea">
+            <div className="imgs-ideas">
+              <img src="/images/ideas/lilac-jacket.jpg" alt="Campera Lila" />
+            </div>
+            <i
+              className="fa-solid fa-face-smile fa-2xl"
+              style={{ color: "#ffd43b" }}
+            ></i>
+            <h3>Nombre Fan 8</h3>
+          </div>
+          <div className="idea">
+            <div className="imgs-ideas">
+              <img src="/images/ideas/smile-beanie.jpg" alt="Smile Beanie" />
+              <img src="/images/ideas/meow-beanie.jpg" alt="Mewo Beanie" />
+              <img src="/images/ideas/meow-beanie2.jpg" alt="Meow Beanie 2" />
+            </div>
+            <i
+              className="fa-solid fa-face-smile fa-2xl"
+              style={{ color: "#ffd43b" }}
+            ></i>
+            <h3>Nombre Fan 9</h3>
+          </div>
+          <div className="idea">
+            <div className="imgs-ideas">
+              <img src="/images/ideas/rainbow-wall.jpg" alt="Pared Arcoíris" />
+              <img
+                src="/images/ideas/rainbow-wall2.jpg"
+                alt="Pared Arcoíris 2"
+              />
+            </div>
+            <i
+              className="fa-solid fa-face-smile fa-2xl"
+              style={{ color: "#ffd43b" }}
+            ></i>
+            <h3>Nombre Fan 10</h3>
+          </div>
+          <div className="idea">
+            <div className="imgs-ideas">
+              <img src="/images/ideas/sunny.jpg" alt="Pastel Sunny" />
+            </div>
+            <i
+              className="fa-solid fa-face-smile fa-2xl"
+              style={{ color: "#ffd43b" }}
+            ></i>
+            <h3>Nombre Fan 11</h3>
+          </div>
+          <div className="idea">
+            <div className="imgs-ideas">
+              <img src="/images/ideas/xmas-mug.jpg" alt="Taza Navideña" />
+              <img src="/images/ideas/xmas-mug2.jpg" alt="Taza Navideña 2" />
+            </div>
+            <i
+              className="fa-solid fa-face-smile fa-2xl"
+              style={{ color: "#ffd43b" }}
+            ></i>
+            <h3>Nombre Fan 12</h3>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+
+export default FansPage;
---
+++ frontend/src/components/pages/InspiracionPage.js
@@ -0,0 +1,53 @@
+import "../../styles/components/pages/InspiracionPage.css";
+
+const InspiracionPage = (props) => {
+  return (
+    <div className="centrar1">
+    <main className="activo">
+      <h2 className="centrar2">Inspiración del día</h2>
+      <p className="centrar2">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+        blanditiis quisquam omnis optio quae, earum nisi temporibus totam velit
+        voluptas! Odit aliquid consequuntur magni aut, corporis reiciendis
+        itaque fuga aliquam tempore explicabo facilis eaque tempora voluptatem,
+        quos sed excepturi a culpa reprehenderit dignissimos consectetur eveniet
+        cum dolores quasi! Magnam, perferendis explicabo eaque impedit suscipit
+        voluptatum voluptates temporibus maiores dolorem repellat.
+      </p>
+      <div id="grilla">
+        <img src="/images/grilla/aesthetic-nails.jpg" alt="Aesthetic Nails" />
+        <img src="/images/grilla/always-happy.jpg" alt="Always Happy" />
+        <img src="/images/grilla/beach.jpg" alt="Beach" />
+        <img src="/images/grilla/clouds-nailart.jpg" alt="Clouds NailArt" />
+        <img src="/images/grilla/dalmatian.jpg" alt="Dalmatian" />
+        <img src="/images/grilla/girl1.jpg" alt="Girl" />
+        <img src="/images/grilla/green-checker.jpg" alt="Green Checker" />
+        <img src="/images/grilla/hairstyle1.jpg" alt="HairStyle" />
+        <img src="/images/grilla/hamburger.jpg" alt="Kawaii Hamburger" />
+        <img src="/images/grilla/lilac-flowers.jpg" alt="Lilac Flowers" />
+        <img src="/images/grilla/oh-happy-day.jpg" alt="Oh Happy Day" />
+        <img
+          src="/images/grilla/pink-animal-print.jpg"
+          alt="Pink Animal Print"
+        />
+        <img src="/images/grilla/red-checker.jpg" alt="Red Checker" />
+        <img src="/images/grilla/sky-and-rainbow.jpg" alt="Sky and Rainbow" />
+        <img
+          src="/images/grilla/smiey-face-clothing.jpg"
+          alt="Smiley Face Clothing"
+        />
+        <img
+          src="/images/grilla/sweater-and-jeans.jpg"
+          alt="Sweater and Jeans"
+        />
+        <img src="/images/grilla/teddy-puffer.jpg" alt="Teddy Puffer" />
+        <img src="/images/grilla/unicorn-pop-tart.jpg" alt="Unicorn Pop-Tart" />
+        <img src="/images/grilla/watermelon.jpg" alt="Watermelon" />
+        <img src="/images/grilla/zara.jpg" alt="Zara Clothing" />
+      </div>
+    </main>
+    </div>
+  );
+};
+
+export default InspiracionPage;
